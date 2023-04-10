b420a27a7e155d9842b92566b743b4e74e5fde70
Tayler Edelen <TaylerLEdelen@gmail.com>
2023-04-10 17:49:51 -0500

    Finished chapter 6 exercise

--- src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
@@ -47,7 +47,10 @@ public class Car {
     }
 
     public void setGasTankLevel(double gasTankLevel) {
-        this.gasTankLevel = gasTankLevel;
+    if(gasTankLevel > this.getGasTankSize()) {
+        throw new IllegalArgumentException("Can't exceed tank size");
+        }
+    this.gasTankLevel = gasTankLevel;
     }
 
     public double getMilesPerGallon() {
@@ -83,6 +86,10 @@ public class Car {
         double gallonsUsed = milesAbleToTravel / this.milesPerGallon;
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
+    }
+
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
     }
 
 }
--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -2,9 +2,11 @@ package org.launchcode.java.demos.lsn5unittesting.test;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.java.demos.lsn5unittesting.main.Car;
-
-import static org.junit.Assert.*;
-//import org.launchcode.java.demos.lsn5unittesting.main.Car;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
 
 public class CarTest {
 
@@ -29,17 +31,45 @@ public class CarTest {
     }
 
     //Write another version of testInitialGasTank() using assertFalse(), comparing the value to 0.
+    @Test
     public void testInitialGasTankFalse() {
         assertFalse(test_car.getGasTankLevel() == 0);
     }
 
     //Write another version of testInitialGasTank() using assertTrue()
+    @Test
     public void testInitialGasTankTrue() {
         assertTrue(test_car.getGasTankLevel() == 10);
     }
 
     //TODO: gasTankLevel is accurate after driving within tank range
+
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001); //delta means "is it + or - .001 from what we expect?"
+    }
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
+    @Test
+    public void testGasTankAfterExceedingTankRange() {
+        //simulate the Car travelling farther than it's gasTankLevel allows
+        //Total Miles = miles per gallon * tank size
+        //Total miles we can travel is 500 (10 * 50 from test_car)
+        //Expected odometer is 500 when tank is empty
+        test_car.drive(600);
+        assertEquals(500, test_car.getOdometer(), .001); //delta means "is it + or - .001 from what we expect?"
+
+    }
+
     //TODO: can't have more gas than tank size, expect an exception
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+        test_car.addGas(5);
+        fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank.");
+    }
+
 
 }
