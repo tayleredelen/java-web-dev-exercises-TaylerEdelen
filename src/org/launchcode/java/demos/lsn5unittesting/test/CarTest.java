2d87adbef04e58fcd5cbadf221838fe413f38e9b
Tayler Edelen <TaylerLEdelen@gmail.com>
2023-04-10 17:56:15 -0500

    Corrected chapter 6 exercise

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -6,11 +6,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import org.launchcode.java.demos.lsn5unittesting.main.Car;
 
 public class CarTest {
 
-    Car test_car;
+//    Car test_car;
     @Before
     public void createCarObject() {
         Car test_car = new Car("Toyota", "Prius", 10, 50);
@@ -18,27 +17,30 @@ public class CarTest {
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
 
-//    @Test
-//    public void emptyTest() {
-//        assertEquals(10,10,.001);
-//    }
+    @Test
+    public void emptyTest() {
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
+        assertEquals(10,10,.001);
+    }
 
     //TODO: constructor sets gasTankLevel properly
     @Test
     public void testInitialGasTank() {
-//        Car test_car = new Car("Toyota", "Prius", 10, 50);
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
         assertEquals(10, test_car.getGasTankLevel(), .001);
     }
 
     //Write another version of testInitialGasTank() using assertFalse(), comparing the value to 0.
     @Test
     public void testInitialGasTankFalse() {
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
         assertFalse(test_car.getGasTankLevel() == 0);
     }
 
     //Write another version of testInitialGasTank() using assertTrue()
     @Test
     public void testInitialGasTankTrue() {
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
         assertTrue(test_car.getGasTankLevel() == 10);
     }
 
@@ -46,6 +48,7 @@ public class CarTest {
 
     @Test
     public void testGasTankAfterDriving() {
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
         test_car.drive(50);
         assertEquals(9, test_car.getGasTankLevel(), .001); //delta means "is it + or - .001 from what we expect?"
     }
@@ -58,6 +61,7 @@ public class CarTest {
         //Total Miles = miles per gallon * tank size
         //Total miles we can travel is 500 (10 * 50 from test_car)
         //Expected odometer is 500 when tank is empty
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
         test_car.drive(600);
         assertEquals(500, test_car.getOdometer(), .001); //delta means "is it + or - .001 from what we expect?"
 
@@ -67,6 +71,7 @@ public class CarTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testGasOverfillException() {
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
         test_car.addGas(5);
         fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank.");
     }
